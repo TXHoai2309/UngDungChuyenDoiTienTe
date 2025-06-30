4c4a514e65f72b56d4e45785b4ef5b5f5fbcff3f
DuyBinh <duybinh01122005@gmail.com>
2025-06-30 13:59:32 +0700

    binh viet lan 6

--- app/src/main/java/com/example/ungdungchuyendoitiente/MainActivity.java
+++ app/src/main/java/com/example/ungdungchuyendoitiente/MainActivity.java
@@ -34,6 +34,6 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-        // binh viet lan 5
+        // binh viet lan 6
     }
 }
