869b8c7bb9e3d599543d20cd3d389077ee4b801a
Tô Xuân Hoài <todien1432@gmail.com>
2025-07-01 14:27:39 +0700

    Hoài commit 14:46, 7/1

---
+++ app/src/main/java/com/example/ungdungchuyendoitiente/ChonDonViTienTe.java
@@ -0,0 +1,24 @@
+package com.example.ungdungchuyendoitiente;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class ChonDonViTienTe extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_chon_don_vi_tien_te);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
